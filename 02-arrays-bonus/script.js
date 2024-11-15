05db82495f8636f3d1fb51995e4230ed3f4d24ba
andreapanico12 <andreapanico12@gmail.com>
2024-11-15 15:40:12 +0100

    fix: add comments in bonus exercise file

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -11,8 +11,12 @@ const teachers = [
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
 
+// =====SOLUZIONE CON METODO CERCATO IN DOCUMENTAZIONE====
+
 // const reversedTeachers = teachers.toReversed();
 // console.log(reversedTeachers)
+
+// =====SOLUZUONE CON LOGICA "FOR"======
 
 const reversedTeachers = [];
 for (i=teachers.length -1; i >= 0; i--){
@@ -25,8 +29,12 @@ console.log(reversedTeachers)
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 
+// =====SOLUZIONE CON METODO CERCATO IN DOCUMENTAZIONE====
+
 // const longNames = teachers.filter(name => name.length >= 5);
 // console.log(longNames)
+
+// =====SOLUZUONE CON LOGICA "FOR"======
 
 const longNames = [];
 for (i=0; i< teachers.length; i++){
