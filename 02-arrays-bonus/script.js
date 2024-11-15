cbccccfe7e6a131376df7155430a20510392e026
andreapanico12 <andreapanico12@gmail.com>
2024-11-15 15:33:32 +0100

    fix: first bonus exercise with cicle "for" logic

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,13 +10,23 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.toReversed();
+
+// const reversedTeachers = teachers.toReversed();
+// console.log(reversedTeachers)
+
+const reversedTeachers = [];
+for (i=teachers.length -1; i >= 0; i--){
+  reversedTeachers.push(teachers[i])
+}
 console.log(reversedTeachers)
+  
+
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = teachers.filter(name => name.length >= 5);
-console.log(longNames)
+
+// const longNames = teachers.filter(name => name.length >= 5);
+// console.log(longNames)
 
 // 3. Rimuovi 'Ed' dall'array teachers
 teachers.splice(1,1)
